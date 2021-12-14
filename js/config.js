45c4768e257a047d6f3fb856823be317605b3022
minminoo10 <95972839+minminoo10@users.noreply.github.com>
2021-12-14 07:38:30 -0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,9 @@
+let site_detail = {
+  'site_name': "Composition',
+  'tagline': 'A site script for static website with dynamic data',
+  'site_home': "https://miniminoo10.github.io',
+  'blog_home': "https://miniminoo10.github.io/composition',
+  'products':'products.html'
+}  
 
+export {site_detail}
